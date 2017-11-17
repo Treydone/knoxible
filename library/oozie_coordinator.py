a428625aadceb4f6232f5d5629638a078e4b9e15
Vincent Devillers <vincent.devillers@layer4.fr>
2017-11-17 08:57:59 +0100

    Update Oozie configuration

--- library/oozie_coordinator.py
+++ library/oozie_coordinator.py
@@ -35,12 +35,9 @@ EXAMPLES = '''
 #
 # python core, ansible imports
 #
+import xml.etree.ElementTree as ET
 from ansible.module_utils.basic import *
-
-#
-# custom imports
-#
-from io import StringIO
+from io import StringIO, BytesIO
 
 DEPENDENCIES_OK = True  # use flag as we want to use module.fail_json for errors
 try:
@@ -61,7 +58,8 @@ def kill(baseurl, auth, id):
     else:
         module.fail_json(msg='Error while killing job'.format(id))
 
-def write_property(xml,name,value):
+
+def write_property(xml, name, value):
     xml.write('<property>\n')
     xml.write('<name>')
     xml.write(name)
@@ -71,52 +69,98 @@ def write_property(xml,name,value):
     xml.write('</value>\n')
     xml.write('</property>\n')
 
+
 def write_properties(xml, properties):
     xml.write('<?xml version="1.0" encoding="UTF-8"?>\n<configuration>\n')
     if properties is not None:
         for name in properties:
             value = properties[name]
-            write_property(xml,name,value)
+            write_property(xml, name, value)
     xml.write('</configuration>\n')
 
+def download(module, webdhdfsurl, auth, strict, coordinatorPath):
+    url = '{}{}?op=OPEN'.format(webdhdfsurl, coordinatorPath)
+    open_req = requests.get(url, auth=auth, allow_redirects=False, verify=strict)
+    if open_req.status_code == 307:
+        location = open_req.headers['Location'];
+        read_req = requests.get(location, auth=auth, verify=strict) #, allow_redirects=False, stream=True)
+        if read_req.status_code == 200:
+            return BytesIO(read_req.content)
+            #for chunk in read_req.iter_content(chunk_size=65536):
+            #    content.write(chunk)
+            #    module.fail_json(msg=chunk)
+        else:
+            module.fail_json(msg='Cannot open datanode location {}, got {}'.format(location, open_req.status_code))
+    else:
+        module.fail_json(msg='Cannot open path {}, got {}'.format(coordinatorPath, open_req.status_code))
+
+
 def run(module):
-    baseurl = module.params['url'] + '/oozie/v1'
+    baseurl = module.params['url']
+    ooziebaseurl = baseurl + '/oozie/v1'
+    webdhdfsurl = baseurl + '/webhdfs/v1'
     user = module.params['user']
     password = module.params['password']
     strict = module.params['strict']
 
     file = module.params['file']
-    #state = module.params['state']
-
-    properties=dict(line.strip().split('=') for line in open(file))
+    # state = module.params['state']
 
     auth = HTTPBasicAuth(user, password)
 
-    url = '{}/jobs?filter=jobtype%3Dcoordinator%3Bname={}'.format(baseurl, name)
+    # Read properties
+    properties = dict(line.strip().split('=') for line in open(file))
+    coordinatorPath = properties.get('oozie.coord.application.path')
+
+    # Coordinator is already deployed?
+    url = '{}{}?op=GETFILESTATUS'.format(webdhdfsurl, coordinatorPath)
     req = requests.get(
         url,
         auth=auth,
         verify=strict)
+    if req.status_code == 404:
+        # Coordinator file is missing -> error
+        module.fail_json(msg='Cannot find coordinator at {}'.format(coordinatorPath))
     if req.status_code == 200:
-        # Kill all the coordinators found
-        for job in req.json()['jobs']:
-            kill(baseurl, auth, job['id'])
+        # Coordinator file exist, check if already deployed
 
-        xml = StringIO()
-        write_properties(xml, properties)
-        req = requests.post(
+        # Download remote coordinator file
+        content = download(module, webdhdfsurl, auth, strict, coordinatorPath)
+
+        # Find coordinator name
+        content_as_string = content.getvalue()
+        try:
+            coordinatorName = ET.parse(content_as_string).find(".").get('name')
+        except:
+            module.fail_json(msg='Cannot parse content {}, got {}'.format(content_as_string, sys.exc_info()[0]))
+
+        # Find coordinator from name
+        url = '{}/jobs?filter=jobtype%3Dcoordinator%3Bname={}'.format(ooziebaseurl, coordinatorName)
+        req = requests.get(
             url,
-            data=xml.getvalue(),
             auth=auth,
             verify=strict)
+        if req.status_code == 200:
+            # Kill all the coordinators found
+            for job in req.json()['jobs']:
+                kill(ooziebaseurl, auth, job['id'])
 
-        if req.status_code == 201:
-            module.exit_json(changed=True, msg='Job deployed', id=req.json()['id'])
+            # Deploy coordinator
+            xml = StringIO()
+            write_properties(xml, properties)
+            req = requests.post(
+                url,
+                data=xml.getvalue(),
+                auth=auth,
+                verify=strict)
+
+            if req.status_code == 201:
+                module.exit_json(changed=True, msg='Job deployed', id=req.json()['id'])
+            else:
+                module.fail_json(msg='Error while deploying the job'.format(id))
         else:
-            module.fail_json(msg='Error while deploying the job'.format(id))
-    else:
-        module.fail_json(msg='Error while finding info about coordinators')
-
+            module.fail_json(msg='Error while finding info about coordinators, got {}: {}'.format(req.status_code, req.text))
+    module.fail_json(msg='Cannot get file status for path {}, got {}'.format(coordinatorPath, req.status_code))
 
 def main():
     module = AnsibleModule(
